50224ccb0d202abbb2fe0ad7a3632c8d82efe2aa
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2017-09-01 00:17:07 +1200

    calculations working

--- main.js
+++ main.js
@@ -1,56 +1,137 @@
+//var calculation = Number(inputField.value);
 var inputField = document.getElementById("inputF");
-var calculation = Number(inputField.value);
-console.log("0", calculation)
+inputField.value = "0"
+var hiddenString = ""
 
-/*
-function numberModerator () {
-  if numberPressed beforehand then += else =
+function removeFirstZero () {
+  for (var i = 0; i < inputField.value.length; i++) {
+    if (inputField.value[0] = "0") {
+      String.prototype.replaceAt = function(index, replacement) {
+          return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+      }
+      var newInputField = inputField.value.replaceAt(0, " ");
+      inputField.value = newInputField;
+    }
+  }
 }
 
-when X take the input field into var entry1 or entry[i]
-until you hit = button it goes entry1 + operator + entry2 + operator + entry3
-*/
+function numBtnClicked () {
+  console.log("Number Button Clicked");
+  console.log("hidden string: " + hiddenString);
+  return true;
+}
 
-// var btnEquals = function (evt) {
-//   console.log("equals button hit");
-//   return;
-// }
-// console.log(btnEquals());
+function optrBtnClicked () {
+  console.log("Operator Button Clicked");
+  inputField.value = "0";
+  console.log("hidden string: " + hiddenString);
+  return true;
+}
+
+
+
+// Operator Buttons
+
+function btnEquals () {
+  var answer = eval(hiddenString);
+  console.log("hidden string answer: " + answer);
+  inputField.value = answer;
+}
+
+function btnDiv () {
+  hiddenString += "/";
+  optrBtnClicked();
+}
+
+function btnMul () {
+  hiddenString += "*";
+  optrBtnClicked();
+}
+
+function btnMin () {
+  hiddenString += "-";
+  optrBtnClicked();
+}
+
+function btnAdd () {
+  hiddenString += "+";
+  optrBtnClicked();
+}
+
+
+// Other Buttons
+
+function btnAC () {
+  inputField.value = "0";
+  hiddenString = "";
+  console.log("hidden string: " + hiddenString);
+}
+
+
+// Number Buttons
 
 function btnNumOne (evt) {
-  if (!btnEquals) {
-    console.log("equals not hit");
-    inputField.value += "1";
-  } else {
-    inputField.value = "1";
-  }
-  //return numberPressed true;
+  //inputField.value = "0";
+  inputField.value += "1";
+  hiddenString += "1";
+  numBtnClicked();
+  removeFirstZero();
 }
-
 function btnNumTwo (evt) {
-  inputField.value = "2";
+  inputField.value += "2";
+  hiddenString += "2";
+  removeFirstZero();
+  numBtnClicked()
 }
 function btnNumThree (evt) {
-  inputField.value = "3";
+  inputField.value += "3";
+  hiddenString += "3";
+  removeFirstZero();
+  numBtnClicked()
 }
 function btnNumFour (evt) {
-  inputField.value = "4";
+  inputField.value += "4";
+  hiddenString += "4";
+  removeFirstZero();
+  numBtnClicked()
 }
 function btnNumFive (evt) {
-  inputField.value = "5";
+  inputField.value += "5";
+  hiddenString += "5";
+  removeFirstZero();
+  numBtnClicked()
 }
 function btnNumSix (evt) {
-  inputField.value = "6";
+  inputField.value += "6";
+  hiddenString += "6";
+  removeFirstZero();
+  numBtnClicked()
 }
 function btnNumSeven (evt) {
   inputField.value += "7";
+  hiddenString += "7";
+  removeFirstZero();
+  numBtnClicked()
 }
 function btnNumEight (evt) {
-  inputField.value = "8";
+  inputField.value += "8";
+  hiddenString += "8";
+  removeFirstZero();
+  numBtnClicked()
 }
 function btnNumNine (evt) {
-  inputField.value = "9";
+  inputField.value += "9";
+  hiddenString += "9";
+  removeFirstZero();
+  numBtnClicked()
 }
 function btnNumZero (evt) {
-  inputField.value = "0";
+  inputField.value += "0";
+  hiddenString += "0";
+  removeFirstZero();
+  numBtnClicked()
+}
+function btnNumDec (evt) {
+  inputField.value += ".";
+  hiddenString += ".";
 }
